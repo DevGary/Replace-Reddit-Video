7a1111455f46c4a02cf95b2360046f5b11473a40
Gary <ImDevGary@gmail.com>
2020-12-26 10:44:02 -0800

    RRV-1: Parse reddit video url id from reddit video player element instead of page metadata

--- replace-reddit-video.js
+++ replace-reddit-video.js
@@ -2,26 +2,28 @@
 // TODO: Doesn't work if post loaded as modal instead of individual page
 
 setTimeout(function () {
-    var videoUrl = document.querySelector("meta[property='og:video']").getAttribute("content");
-    var videoUrlId = videoUrl.split("v.redd.it/")[1].split("/")[0];
-    var videoFallbackUrl = "https://v.redd.it/" + videoUrlId + "/DASH_480.mp4?source=fallback";
 
-    videoUrl = videoFallbackUrl;
+    let redditNativeVideoElements = document.getElementsByTagName("video");
+    let redditNativeVideoElementsSourceHtml = redditNativeVideoElements[0].getElementsByTagName("source")[0].outerHTML;
+    
+    let videoUrlId = redditNativeVideoElementsSourceHtml.split("src=\"https://v.redd.it/")[1].split("/")[0];
+    let videoFallbackUrl = "https://v.redd.it/" + videoUrlId + "/DASH_480.mp4?source=fallback";
+    let videoUrl = videoFallbackUrl;
 
     let containerElementId = "replace-reddit-video-container";
     let videoElementId = "replace-reddit-video-video";
 
-    var videoContainerElement = document.createElement("div");
+    let videoContainerElement = document.createElement("div");
     videoContainerElement.setAttribute("class", containerElementId);
 
-    var video = document.createElement("video");
+    let video = document.createElement("video");
     video.setAttribute("class", videoElementId);
 
     video.autoplay = true;
     video.loop = true;
     video.controls = true;
 
-    var sourceMP4 = document.createElement("source");
+    let sourceMP4 = document.createElement("source");
     sourceMP4.type = "video/mp4";
     sourceMP4.src = videoUrl;
 
@@ -29,7 +31,7 @@ setTimeout(function () {
 
     videoContainerElement.appendChild(video);
 
-    document.getElementsByTagName("video")[0].parentElement.parentElement.parentElement.replaceWith(videoContainerElement);
+    redditNativeVideoElements[0].parentElement.parentElement.parentElement.replaceWith(videoContainerElement);
 
     video.play();
 }, 1);
